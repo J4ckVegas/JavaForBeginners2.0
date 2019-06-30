19785d311e295f84aff64629f9a833e97f04c158
J4ckVegas <sickjackvegas@gmail.com>
2019-06-30 23:00:33 +0300

    Initial commit

---
+++ src/main/java/Task01.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+
+public class Task01 {
+    public static void main(String[] args) {
+        /** Метод сложения двух дробных чисел
+         * @author Volokhov-YUV */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Введите операцию:");
+        char operation = scanner.next().charAt(0);
+
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                System.out.printf("%.4f", result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.printf("%.4f", result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.printf("%.4f", result);
+                break;
+            case '/':
+                result = num1 / num2;
+                System.out.printf("%.4f", result);
+                break;
+        }
+        scanner.close();
+    }
+}
